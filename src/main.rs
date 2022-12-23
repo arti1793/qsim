967ba64c5c7a79e54e2573ec920424021e780098
arti1793 <arti1793@gmail.com>
2022-12-23 16:55:54 +0300

    ~

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,34 @@ use bevy::{
     app::App,
     prelude::{BuildChildren, Camera3dBundle, Commands, Query, Transform, Vec3, With},
     transform::TransformBundle,
+    ui::PositionType,
     DefaultPlugins,
 };
 use bevy_rapier3d::{
-    prelude::{Collider, NoUserData, RapierPhysicsPlugin, Restitution, RigidBody},
+    prelude::{
+        AdditionalMassProperties, Collider, ExternalForce, MassProperties, NoUserData,
+        RapierPhysicsPlugin, Restitution, RigidBody,
+    },
     render::RapierDebugRenderPlugin,
+};
+
+struct Config {
+    l: f32,   // м радиус рамы
+    t: f32,   // м толщина рамы
+    rs: f32,  //м радиус центральной "сферы"
+    ms: f32,  // г масса центральной сферы
+    ml: f32,  // г масса рамы
+    mm: f32,  // г масса двигателя
+    mmm: f32, // г радиус сферы двигателя
+}
+const CONFIG: Config = Config {
+    l: 0.55,   // м радиус рамы
+    t: 0.02,   // м толщина рамы
+    ml: 0.25,  // масса одной рамы
+    rs: 0.070, //м радиус центральной "сферы"
+    ms: 200.0, // г масса центральной сферы
+    mm: 0.56,  // масса двигателя
+    mmm: 0.07, // радиус сферы двигателя
 };
 
 fn main() {
@@ -34,35 +57,123 @@ fn setup_physics(mut commands: Commands) {
         .spawn(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)))
         .insert(Collider::cuboid(100.0, 0.1, 100.0));
 
-    /* Create the bouncing ball. */
-    commands
-        .spawn(TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)))
-        .insert(RigidBody::Dynamic)
-        .insert(Collider::ball(0.2))
-        .insert(Restitution::coefficient(0.7));
-
-    // let joint = FixedJointBuilder::new().local_anchor1(Vec3::new(0.0, 0.0, -2.0));
-    // commands
-    //     .spawn(TransformBundle::from(Transform::from_xyz(0.0, 10.0, 0.0)))
-    //     .insert(RigidBody::Dynamic)
-    //     .insert(Collider::cuboid(3.0, 1.0, 1.0))
-    //     .insert(RigidBody::Dynamic)
-    //     .insert(Collider::cuboid(1.0, 3.0, 1.0));
-
     commands
         .spawn(RigidBody::Dynamic)
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 10.0, 0.0)))
+        .insert(AdditionalMassProperties::Mass(0.005))
+        // .insert(ExternalForce {
+        // //     // force: Vec3 {
+        // //     //     x: 0.0,
+        // //     //     y: 100000.0,
+        // //     //     z: 0.0,
+        // //     // },
+        // //     torque: Vec3 {
+        // //         x: 1000000.0,
+        // //         y: 0.0,
+        // //         z: 0.0,
+        // //     },
+        //     ..Default::default()
+        // })
+        .insert(Restitution::coefficient(0.7))
         .with_children(|children| {
             children
-                .spawn(Collider::cuboid(3.0, 1.0, 1.0))
+                .spawn(Collider::cuboid(CONFIG.l, CONFIG.t, CONFIG.t))
+                .insert(AdditionalMassProperties::Mass(CONFIG.ml))
                 .insert(TransformBundle::from(Transform::from_xyz(0.0, 0.0, 0.0)));
             children
-                .spawn(Collider::cuboid(1.0, 1.0, 3.0))
+                .spawn(Collider::cuboid(CONFIG.t, CONFIG.t, CONFIG.l))
+                .insert(AdditionalMassProperties::Mass(CONFIG.ml))
                 .insert(TransformBundle::from(Transform::from_xyz(0.0, 0.0, 0.0)));
+
+            children
+                .spawn(Collider::ball(CONFIG.rs))
+                .insert(TransformBundle::from(Transform::from_xyz(0.0, 0.0, 0.0)))
+                .insert(AdditionalMassProperties::Mass(CONFIG.ms));
+
+            children
+                .spawn(Collider::ball(CONFIG.mmm))
+                .insert(TransformBundle::from(Transform::from_xyz(
+                    CONFIG.l, 0.0, 0.0,
+                )))
+                .insert(AdditionalMassProperties::Mass(CONFIG.mm));
+            // .insert(ExternalForce {
+            // force: Vec3 {
+            //     x: 1000000.0,
+            //     y: 0.0,
+            //     z: 0.0,
+            // },
+            // torque: Vec3 {
+            //     x: 0.0,
+            //     y: 1.0,
+            //     z: 0.0,
+            // },
+            // ..Default::default()
+            // });
+            children
+                .spawn(Collider::ball(CONFIG.mmm))
+                .insert(TransformBundle::from(Transform::from_xyz(
+                    -CONFIG.l, 0.0, 0.0,
+                )))
+                .insert(AdditionalMassProperties::Mass(CONFIG.mm));
+            // .insert(ExternalForce {
+            // force: Vec3 {
+            //     x: 0.0,
+            //     y: 100000.0,
+            //     z: 0.0,
+            // },
+            // torque: Vec3 {
+            //     x: 0.0,
+            //     y: 1.0,
+            //     z: 0.0,
+            // },
+            // ..Default::default()
+            // });
+            children
+                .spawn(Collider::ball(CONFIG.mmm))
+                .insert(TransformBundle::from(Transform::from_xyz(
+                    0.0, 0.0, CONFIG.l,
+                )))
+                .insert(AdditionalMassProperties::Mass(CONFIG.mm));
+            // .insert(ExternalForce {
+            // force: Vec3 {
+            //     x: 0.0,
+            //     y: 100000.0,
+            //     z: 0.0,
+            // },
+            // torque: Vec3 {
+            //     x: 0.0,
+            //     y: 1.0,
+            //     z: 0.0,
+            // },
+            // ..Default::default()
+            // });
+            children
+                .spawn(Collider::ball(CONFIG.mmm))
+                .insert(TransformBundle::from(Transform::from_xyz(
+                    0.0, 0.0, -CONFIG.l,
+                )))
+                .insert(AdditionalMassProperties::Mass(CONFIG.mm));
+            // .insert(ExternalForce {
+            //     force: Vec3 {
+            //         x: 0.0,
+            //         y: 100000.0,
+            //         z: 0.0,
+            //     },
+            //     torque: Vec3 {
+            //         x: 0.0,
+            //         y: 0.0,
+            //         z: 0.0,
+            //     },
+            //     ..Default::default()
+            // });
         });
 }
 
 fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
     for transform in positions.iter() {
-        println!("Ball altitude: {}", transform.translation.y);
+        println!(
+            "Ball altitude: {}, {}",
+            transform.translation.y, transform.rotation.y
+        );
     }
 }
